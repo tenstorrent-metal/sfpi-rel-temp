7eec10d62746549005e0ed67636f83285be5449f
Paul Keller <pkeller@tenstorrent.com>
2021-09-28 22:27:38 +0000

    First cut compiler

---
+++ compiler/lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/pkeller/src/sfpi/tt-gcc/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/localhome/pkeller/src/sfpi/compiler --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/localhome/pkeller/src/sfpi/compiler/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././riscv-gcc --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" }, { "tune", "rocket" } };
